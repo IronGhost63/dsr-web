3219afa80f6ee340218992860b022cb056d84960
Jirayu Yingthawornsuk <me@jirayu.in.th>
2017-11-09 17:04:26 +0700

    new sidebar

--- src/sidebar.php
+++ src/sidebar.php
@@ -3,7 +3,7 @@ function wp63_register_sidebars(){
 	register_sidebar( array(
 		'name' => __( 'Main Sidebar', 'dsr' ),
 		'id' => 'sidebar-single',
-		'description' => __( 'Widgets in this area will be shown on all posts and pages.', 'dsr' ),
+		'description' => __( 'Default sidebar', 'dsr' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div></aside>',
 		'before_title'  => '<h4 class="widget-title">',
@@ -13,7 +13,17 @@ function wp63_register_sidebars(){
 	register_sidebar( array(
 		'name' => __( 'Page Sidebar', 'dsr' ),
 		'id' => 'sidebar-page',
-		'description' => __( 'Widgets in this area will be shown on all posts and pages.', 'dsr' ),
+		'description' => __( 'Generic Page Sidebar', 'dsr' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div></aside>',
+		'before_title'  => '<h4 class="widget-title">',
+		'after_title'   => '</h4><div class="widget-content">',
+	) );
+	
+	register_sidebar( array(
+		'name' => __( 'About Sidebar', 'dsr' ),
+		'id' => 'sidebar-about',
+		'description' => __( 'Sidebar for About section', 'dsr' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div></aside>',
 		'before_title'  => '<h4 class="widget-title">',
