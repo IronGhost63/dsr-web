9449a3ce162c355c819de0c02711f82d2418bb57
Jirayu Yingthawornsuk <me@jirayu.in.th>
2017-11-22 01:51:11 +0700

    change footer to single line

--- template-parts/global-footer.php
+++ template-parts/global-footer.php
@@ -1,20 +1,9 @@
 	<footer id="footer">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-2">
-					<img src="<?php asset_path();?>/logo-transparent.png" alt="" class="footer-logo">
-				</div>
-				<div class="col-md-7"></div>
-				<div class="col-md-3">
-					<h6>ติดต่อโรงเรียน</h6>
-					<p class="address">
-					โรงเรียนเทพศิรินทร์ร่มเกล้า เลขที่ 2 ซอย ไอซีดี 8 แขวงคลองสามประเวศ เขตลาดกระบัง กรุงเทพมหานคร 10520
-					</p>
-					<p class="phone">
-						<i class="fa fa-phone" aria-hidden="true"></i> โทรศัพท์ 0 2737 8919 - 20
-					</p>
-					<p class="fax">
-						<i class="fa fa-fax" aria-hidden="true"></i> โทรสาร 0 2360 9287 
+				<div class="col">
+					<p class="address text-center">
+					โรงเรียนเทพศิรินทร์ร่มเกล้า เลขที่ 2 ซอย ไอซีดี 8 แขวงคลองสามประเวศ เขตลาดกระบัง กรุงเทพมหานคร 10520 <i class="fa fa-phone" aria-hidden="true"></i> โทรศัพท์ 0 2737 8919 - 20 <i class="fa fa-fax" aria-hidden="true"></i> โทรสาร 0 2360 9287
 					</p>
 				</div>
 			</div>
