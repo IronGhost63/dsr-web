bdf2b95b882098646892c5c3154d671794b21082
Jirayu Yingthawornsuk <me@jirayu.in.th>
2019-08-20 23:14:15 +0700

    remove unused files

--- wp-content/themes/test/public/js/main.js
+++
@@ -1,8 +0,0 @@
-new WOW().init();
-
-jQuery(document).ready(function ($) {
-	var $all_oembed_videos = $("iframe[src*='youtube']");
-	$all_oembed_videos.each(function () {
-		$(this).removeAttr('height').removeAttr('width').wrap("<div class='embed-container'></div>");
-	});
-});
